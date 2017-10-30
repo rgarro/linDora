32a493482a0e307e2c5bd4036d7f38ac08402439
rgarro <rgarro@gmail.com>
2017-10-29 18:49:12 -0600

    это дом ...

---
+++ Assets/linDora/Scripts/CarController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+
+	public WheelCollider[] WColForward; //2
+	public WheelCollider[] WColBack;  //3
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
