3aecb384b0574973237b01ef16e5d47650d14796
rolando <rgarro@gmail.com>
2021-04-17 10:56:25 -0600

    Animator.GotoState: State could not be found ..

---
+++ Assets/simpleAnimMotionPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class simpleAnimMotionPlayer : MonoBehaviour
+{
+    private Animator arnAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("here with the anim");
+        this.arnAnim = GetComponent<Animator>();
+        this.arnAnim.Play("turningradar");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
