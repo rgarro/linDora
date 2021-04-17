c7514144852790be05a0b51a03c63bbd469e2060
rolando <rgarro@gmail.com>
2021-04-17 13:36:53 -0600

    playing fbx animation update

--- Assets/simpleAnimMotionPlayer.cs
+++ Assets/simpleAnimMotionPlayer.cs
@@ -2,21 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/**
+* Will play state from animator 
+*
+*/
 public class simpleAnimMotionPlayer : MonoBehaviour
 {
     private Animator arnAnim;
+    private AnimatorClipInfo[] m_AnimatorClipInfo;
+    public string stateName = "TurningState";
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("here with the anim");
         this.arnAnim = GetComponent<Animator>();
-        this.arnAnim.Play("turningradar");
+        //Get the animator clip information from the Animator Controller
+        m_AnimatorClipInfo = arnAnim.GetCurrentAnimatorClipInfo(0);
+        //Output the name of the starting clip
+        Debug.Log("Starting clip : " + m_AnimatorClipInfo[0].clip);
+        this.arnAnim.Play(this.stateName);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (null != this.arnAnim)
+            {
+                Debug.Log(this.stateName);
+                this.arnAnim.Play(this.stateName, 0, 0.25f);
+            }
+        }
     }
 }
